0d86bcaa6f48329dc2abe00fef6716849c7cc71a
windowsazure <windowsazure>
2013-10-23 22:46:05 +0000

    Committing update from request /scmvfs/service/api/sync.js

--- service/api/sync.js
+++ service/api/sync.js
@@ -2,12 +2,13 @@ exports.post = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
     //   var tables = request.service.tables;
     //   var push = request.service.push;
-
+    console.log("/sync POST with request: " + request);
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
 
 exports.get = function(request, response) {
     var tables = request.service.tables;
+    console.log("/sync GET with request: " + request);
     
     response.send(statusCodes.OK, { message : 'Hello World!' });
 };
