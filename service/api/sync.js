b7c6f5248fba54e8025a9ef844d377623dc7ba70
Shane Church <shane@s-church.net>
2013-10-30 19:40:00 -0600

    Added user security code

--- service/api/sync.js
+++ service/api/sync.js
@@ -1,4 +1,5 @@
 var Enumerable = require("linq");
+var uuid = require("node-uuid");
 
 exports.post = function (request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
@@ -8,35 +9,45 @@ exports.post = function (request, response) {
         console.log("/sync POST with request '%j' by user %s", [request.body, request.user]);
         var body = request.body, count = 0, results = [];
         if (!body.lastSyncDate ||
-                !body.items) {
+                !body.items ||
+                !body.email) {
             console.error("Invalid request body: %j", request.body);
             response.send(statusCodes.BAD_REQUEST, {message: "Invalid request body"});
         }
-        body.items.forEach(function (item) {
-            var options = {
-                tableName: item.tableName,
-                idField: item.keyField,
-                user: request.user,
-                userIds: [],
-                values: item.values,
-                lastSyncDate: body.lastSyncDate,
-                success: function (serverChanges) {
-                    results.push(serverChanges);
-                    count = count + 1;
-                    if (count === body.items.length) {
-                        response.send(statusCodes.OK, results);
-                    }
-                },
-                error: function (error, statusCode) {
-                    console.error("Error occurred processing request '%j' from user '%j':\n\n" + error, request.body, request.user);
-                    if (!statusCode) {
-                        throw { statusCode: statusCodes.INTERNAL_SERVER_ERROR, error: error };
-                    } else {
-                        throw { statusCode: statusCode, error: error };
-                    }
-                }
-            };
-            processClientChanges(options, request);
+
+        GetUserAppSyncId({
+            email: body.email,
+            success: function (appSyncId) {
+                body.items.forEach(function (item) {
+                    var options = {
+                        tableName: item.tableName,
+                        idField: item.keyField,
+                        user: request.user,
+                        appSyncId: appSyncId,
+                        values: item.values,
+                        lastSyncDate: body.lastSyncDate,
+                        success: function (serverChanges) {
+                            results.push(serverChanges);
+                            count = count + 1;
+                            if (count === body.items.length) {
+                                response.send(statusCodes.OK, results);
+                            }
+                        },
+                        error: function (error, statusCode) {
+                            console.error("Error occurred processing request '%j' from user '%j':\n\n" + error, request.body, request.user);
+                            if (!statusCode) {
+                                throw { statusCode: statusCodes.INTERNAL_SERVER_ERROR, error: error };
+                            } else {
+                                throw { statusCode: statusCode, error: error };
+                            }
+                        }
+                    };
+                    processClientChanges(options, request);
+                });
+            },
+            error: function (error) {
+                throw { statusCode: statusCodes.INTERNAL_SERVER_ERROR, error: error };
+            }
         });
     } catch (e) {
         if (e.statusCode) {
@@ -48,15 +59,46 @@ exports.post = function (request, response) {
     }
 };
 
-function GetAuthorizedUserIds(request) {
-    var ids = [];
-    //ids.push(request.user.userId);
-    return ids;
+function GetUserAppSyncId(options) {
+    var table = options.request.service.tables.getTable("AppSyncUsers");
+    table.where({userEmail : options.email })
+        .read({
+            success: function (results) {
+                if(results.length > 0) {
+                    //Insert new user with a new appSyncId GUID
+                    var syncIdBytes = uuid.v4();
+                    var syncId = uuid.unparse(syncIdBytes);
+                    var record = {
+                        appSyncId: syncId,
+                        userId: options.request.user.userId,
+                        userEmail: options.email,
+                        editDateTime: new Date()
+                    };
+                    table.insert(record, {
+                         success: function () {
+                             console.log("Inserted %s into AppSyncUsers with appSyncId {%s}", [options.email, record.appSyncId]);
+                             options.success(record.appSyncId);
+                         },
+                         error: function (error) {
+                             console.log("Error inserting %s into AppSyncUsers with appSyncId {%s}\n\n%s", [options.email, record.appSyncId, error.message]);
+                             options.error(error);
+                         }
+                     });
+                } else {
+                    console.log("User %s exists with appSyncId {%s}", [options.email, record.appSyncId]);
+                    options.success(results[0].appSyncId);
+                }
+            },
+            error: function (error) {
+                console.log("Error retrieving user record %s from AppSyncUsers\n\n\%s", [options.email, error.message]);
+                options.error(error);
+            }
+        });
 }
 
 function processClientChanges(options, request) {
     try {
-        console.log("Processing user (%s) client changes for table: %s", [options.user.userId, options.tableName]);
+        console.log("Processing client changes for table: %s", [options.tableName]);
         var serverChanges = [], serverKeys = [], table = request.service.tables.getTable(options.tableName);
         if (options.values.length > 0) {
             var valuesEnum = Enumerable.From(options.values);
@@ -74,7 +116,7 @@ function processClientChanges(options, request) {
                     if(results.length > 0) {
                         results.forEach(function(item) {
                             serverKeys.push(item[options.idField].toLowerCase());
-                            if(item.userId !== options.user.userId && !(item.userId in options.userIds)) {
+                            if(item.appSyncId !== options.appSyncId) {
                                 console.error("User %j made an unauthorized attempt to edit record {" + item[options.idField] + "} in table " + options.tableName, options.user);
                                 throw { error: new Error("Attempt made to edit unauthorized record"), statusCode: statusCodes.UNAUTHORIZED};
                             } else {
@@ -83,7 +125,7 @@ function processClientChanges(options, request) {
                                 }).FirstOrDefault(null);
                                 if(clientVal && item.editDateTime < clientVal.editDateTime) {
                                     //Update the server entry
-                                    item.userId = options.user.userId;
+                                    item.appSyncId = options.appSyncId;
                                     table.update(item, {
                                         success: function () {
                                             console.log("Updated record {" + item[options.idField] + "} in " + options.tableName);
@@ -94,8 +136,7 @@ function processClientChanges(options, request) {
                                                 var insertOptions = {
                                                     tableName: options.tableName,
                                                     idField: options.idField,
-                                                    user: options.user,
-                                                    userIds: options.userIds,
+                                                    appSyncId: options.appSyncId,
                                                     values: insertValues,
                                                     lastSyncDate: options.lastSyncDate,
                                                     processedKeys: serverKeys,
@@ -120,8 +161,7 @@ function processClientChanges(options, request) {
                                         var insertOptions = {
                                             tableName: options.tableName,
                                             idField: options.idField,
-                                            user: options.user,
-                                            userIds: options.userIds,
+                                            appSyncId: options.appSyncId,
                                             values: insertValues,
                                             lastSyncDate: options.lastSyncDate,
                                             processedKeys: serverKeys,
@@ -141,8 +181,7 @@ function processClientChanges(options, request) {
                         var insertOptions = {
                             tableName: options.tableName,
                             idField: options.idField,
-                            user: options.user,
-                            userIds: options.userIds,
+                            appSyncId: options.appSyncId,
                             values: insertValues,
                             lastSyncDate: options.lastSyncDate,
                             processedKeys: serverKeys,
@@ -163,8 +202,7 @@ function processClientChanges(options, request) {
             var serverOptions = {
                 tableName: options.tableName,
                 idField: options.idField,
-                user: options.user,
-                userIds: options.userIds,
+                appSyncId: options.appSyncId,
                 lastSyncDate: options.lastSyncDate,
                 processedKeys: serverKeys,
                 serverChanges: serverChanges,
@@ -185,7 +223,7 @@ function processClientInserts(options, request) {
         var table = request.service.tables.getTable(options.tableName);
         if(options.values.length > 0) {
             options.values.forEach(function(item) {
-                item.userId = options.user.userId;
+                item.appSyncId = options.appSyncId;
                 item.editDateTime = new Date();
                 delete item.id;
                 table.insert(item, {
@@ -198,8 +236,7 @@ function processClientInserts(options, request) {
                             var serverOptions = {
                                 tableName: options.tableName,
                                 idField: options.idField,
-                                user: options.user,
-                                userIds: options.userIds,
+                                appSyncId: options.appSyncId,
                                 lastSyncDate: options.lastSyncDate,
                                 processedKeys: options.processedKeys,
                                 serverChanges: options.serverChanges,
@@ -220,8 +257,7 @@ function processClientInserts(options, request) {
             var serverOptions = {
                 tableName: options.tableName,
                 idField: options.idField,
-                user: options.user,
-                userIds: options.userIds,
+                appSyncId: options.appSyncId,
                 lastSyncDate: options.lastSyncDate,
                 processedKeys: options.processedKeys,
                 serverChanges: options.serverChanges,
@@ -247,15 +283,9 @@ function processServerChanges(options, request) {
         sql = sql.substr(0, sql.length - 1);
         sql = sql + ")";
     }
-    sql = sql + " and userId in (";
-    sql = sql + "'" + options.user.userId + "',";
-    for(var j=0; j<options.userIds.length; j++) {
-        sql = sql + "'" + options.userIds[j] + "',"
-    }
-    sql = sql.substr(0, sql.length - 1);
-    sql = sql + ")";
+    sql = sql + " and appSyncId = ?"
     console.log(sql);
-    request.service.mssql.query(sql, [options.lastSyncDate], {
+    request.service.mssql.query(sql, [options.lastSyncDate, options.appSyncId], {
         success: function(results) {
             for(var i=0;i<results.length;i++) {
                 options.serverChanges.push(results[i]);
